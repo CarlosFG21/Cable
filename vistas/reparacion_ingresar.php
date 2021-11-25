b833e8cf7c2e2b7757610eb1a6153b63782f1e28
marlonE7 <mat1.6@outlook.com>
2021-11-24 19:39:13 -0600

    subiendo vistas

---
+++ vistas/reparacion_ingresar.php
@@ -0,0 +1,110 @@
+<?php
+
+include ("layout/header.php");
+
+?>
+
+<title>Cablevision | Robles</title>
+
+<?php
+
+include ("layout/nav.php");
+
+?>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Ingresar Reparacion</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <!--/.col (left) -->
+          <!-- right column -->
+          <div class="col-md-12">
+            <!-- general form elements disabled -->
+            <div class="card card-warning">
+              <div class="card-header">
+                <h3 class="card-title">Ingresar</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <form role="form" method="post" action="../crud/ingresarReparacion.php">
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <!-- text input -->
+                      <div class="form-group">
+                        <label>Cliente</label>
+                        <select class="form-control" id="cliente">
+                          <option>Cliente 1</option>
+                          <option>Cliente 2</option>
+                        </select>
+                      </div>
+                    </div>  
+                    <div class="col-sm-6">
+                      <!-- text input -->
+                      <div class="form-group">
+                        <label>Direccion</label>
+                        <select class="form-control" name="direccion" id="direccion">
+                          <option>Dirección 1</option>
+                          <option>Dirección 2</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <!-- text input -->
+                      <div class="form-group">
+                        <label>Empleado</label>
+                        <select class="form-control" name="empleado" id="empleado">
+                          <option>Empleado 1</option>
+                          <option>Empleado 2</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>  
+                  <div class="">
+                  <input type="submit" value="Guardar" class="btn btn-primary" name="btnGuardar" id="btnGuardar">
+                  <a type="submit" class="btn btn-danger" href="cliente.php">Regresar</a>
+                </div>     
+                </form>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+            <!-- general form elements disabled -->
+            
+            <!-- /.card -->
+          </div>
+          <!--/.col (right) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+
+<?php
+
+include ("layout/footer.php");
+
+?>
+  <!--script>
+    /*$(document).ready(function(){
+      $('#cliente').select2();
+    });*/
+  </script-->
